cebe545cdb66933b0ede72f58d55d490f72dea68
jabekl <92928270+jabekl@users.noreply.github.com>
2022-05-14 11:59:34 +0200

    config vars

--- main.py
+++ main.py
@@ -33,4 +33,4 @@ async def get_score(data: Request):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host=0.0.0.0, port=os.getenv('PORT'))
+    uvicorn.run(app, host="0.0.0.0", port=os.getenv('PORT'))
